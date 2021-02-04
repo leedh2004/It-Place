ca911caa22f0d4366d65183410090340e80aaaf0
이도현 <2016025705@hanyang.ac.kr>
2021-02-05 00:37:43 +0900

    DB, S3 테스트 완료 #5

--- node-restful/app.js
+++ node-restful/app.js
@@ -1,6 +1,48 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.SERVER_PORT
+
+var userRoute = require('./route/user')
+var uploadRoute = require('./route/upload')
+var morgan = require('morgan')
+
+// !!! .env 파일 root folder에 생성할 것 !!! .env 파일 notion 참고
+var connection = require('./mysql')
+
+// DB 조회 TEST 완료
+connection.query('SELECT * from user', (error, rows, fields) => {
+  if (error){
+    console.log("error")
+  }
+  console.log('User info is', rows)
+})
+
+// DB INSERT TEST 완료
+/*
+connection.query('INSERT INTO `user` VALUES (2, "Soryung") ', (error, rows, fields) => {
+  if (error){
+    console.log("error")
+  }
+  console.log('INSERT', rows)
+})
+*/
+
+// 라우팅 테스트
+// express built in bodyParser
+app.use(express.json())
+
+// logging
+app.use(morgan('tiny'))
+
+// 정적이미지 제공
+// 요청 url: /assets/test.jpg
+app.use(express.static('public'))
+
+// Routing 테스트
+app.use('/user', userRoute)
+
+// S3 Upload 테스트
+app.use('/upload', uploadRoute)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +51,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
