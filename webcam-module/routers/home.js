5378075c92a65c828e6b6a5af5aa4d39e71e3084
sunajung <jungsuna99@gmail.com>
2021-02-05 03:02:49 +0900

    Add room enter, exit callback #7

--- webcam-module/routers/home.js
+++ webcam-module/routers/home.js
@@ -4,11 +4,9 @@ const router = Router();
 router.get('/', async (req, res) => {
     res.render('index');
 });
-router.get('/master', async (req, res) => {
-    res.render('master');
-});
-router.get('/visiter', async (req, res) => {
-    res.render('visiter');
+router.get('/meeting/:roomNumber', async (req, res) => {
+    const { roomNumber } = req.params;
+    res.render('meeting', { roomNumber });
 });
 
 module.exports = router;
