06bc307be1de9ab3c4a61cadc5addf522ca2b674
Donggyu Kim <ptsaturn68@daum.net>
2021-02-05 00:13:56 +0900

    kotlin 개발 환경 기본 설정
    flutter -> kotlin

---
+++ ITPlace/app/src/main/java/com/example/it_place/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.it_place
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.wv);
+        myWebView.loadUrl("https://www.google.com")
+
+    }
+}
