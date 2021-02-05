2b3623765422e68a14b692679af55994a18edad7
sunajung <jungsuna99@gmail.com>
2021-02-05 23:04:47 +0900

    Add basic css (sub of #4) #17

--- webcam-module/app.js
+++ webcam-module/app.js
@@ -12,9 +12,9 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(morgan('dev'));
-app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 const homeRouter = require('./routers/home.js');
 
